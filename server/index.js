0b1d72ef6550214d447803f5788688dfcd344792
Tori Daughton <toriddaughton@gmail.com>
2022-04-27 14:54:58 -0600

    please work

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
-const express = require(`express`)
-const path = (`path`)
+// const express = require('express')
+// const path = require('path')
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+// })
+
+// const port = process.env.PORT || 4005
+
+// app.listen(port, () =>{
+//     console.log(`Listening on port ${port}`)
+// })
+
+
+
+const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, `../index.html`))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4005
 
-app.listen(port, () => {
+app.listen(port, () =>{
     console.log(`Listening on port ${port}`)
 })
